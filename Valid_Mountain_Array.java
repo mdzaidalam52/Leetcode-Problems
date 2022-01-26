b78ddffe7a42838f44c28c81f30b42004266571b
mdzaidalam52 <mdzaidalam001@gmail.com>
2022-01-26 15:17:10 +0530

    Added problems solved on 25th Jan, 2022

---
+++ Valid_Mountain_Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+        int s = 0;
+        int e = arr.length - 1;
+        while(s + 1 < arr.length - 1 && arr[s] < arr[s + 1]) s++;
+        while(e - 1 > 0 && arr[e] < arr[e - 1]) e--;
+        return s == e;
+    }
+}
+
+// Code by Md Zaid Alam
