c19ef35db9f398236b24e2cd58fbf32b9d61593a
mdzaidalam52 <mdzaidalam001@gmail.com>
2022-02-21 10:24:25 +0530

    Solve Majority_Element with a better solution

--- Majority_Element.java
+++ Majority_Element.java
@@ -1,12 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int a : nums){
-            map.put(a, map.getOrDefault(a, 0) + 1);
-            if(map.get(a) > nums.length/2)
-                return a;
+        int ans = 0;
+        int count = 0;
+        for(int i: nums){
+            if(count == 0)
+                ans = i;
+            count += (i == ans) ? 1 : -1;
         }
-        return -1;
+        return ans;
     }
 }
 
