13e53036dd19fe6ef2fc2b0c1b60af2a5dcd1023
mdzaidalam52 <mdzaidalam001@gmail.com>
2022-03-05 00:57:12 +0530

    Added problems solved on 3rd Mar, 2023

---
+++ Xor_Queries_of_a_Subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i - 1] ^ arr[i];
+        }
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            if (queries[i][0] != 0) {
+                ans[i] = arr[queries[i][1]] ^ arr[queries[i][0] - 1];
+            } else {
+                ans[i] = arr[queries[i][1]];
+            }
+        }
+        return ans;
+    }
+}
+
+// Code by Md Zaid Alam
